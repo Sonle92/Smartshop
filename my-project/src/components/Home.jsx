fa9ee72e8b2e703785c929ad59f65efd7576a894
Wibu2002 <levietson241102@gmail.com>
2023-12-11 13:59:27 +0700

    new code

--- my-project/src/components/Home.jsx
+++ my-project/src/components/Home.jsx
@@ -1,13 +1,107 @@
-import React from 'react'
-import Slide from './Slide'
+import React from "react";
+import Slide from "./Slide";
 import anh1 from "../image/slide1.png";
 import anh2 from "../image/slide6.jpg";
 import anh3 from "../image/slide7.jpg";
+import banner1 from "../image/banner.jpg";
+import banner2 from "../image/banner14s.jpg";
+import banner3 from "../image/banner15.jpg";
+import anh4 from "../image/stevenjob.jpg";
+import anh5 from "../image/anh5.jpg";
+import "../index.css";
+const newsData = [
+  {
+    id: 1,
+    image: banner1,
+  },
+  {
+    id: 2,
+    image: banner2,
+  },
+  {
+    id: 3,
+    image: banner3,
+  },
+];
+
+function NewsItem({ title, content, image }) {
+  return (
+    <div style={{ width: "50%" }}>
+      <img
+        style={{
+          width: "80%",
+          height: "150px",
+          margin: "33px",
+          borderRadius: 15,
+        }}
+        src={image}
+        alt={title}
+      />
+      <div className="news-content"></div>
+    </div>
+  );
+}
 
 export default function Home() {
   return (
     <div>
-      <div><Slide sl1={anh1} sl2={anh2} sl3={anh3} /></div>
+      <div>
+        <Slide sl1={anh1} sl2={anh2} sl3={anh3} />
+      </div>
+      <div className="news-page">
+        <div style={{ display: "flex" }}>
+          {newsData.map((item) => (
+            <NewsItem key={item.id} image={item.image} />
+          ))}
+        </div>
+      </div>
+      <div className="contents">
+        <div
+          style={{
+            backgroundColor: "white",
+            padding: "20px",
+            margin: "20px",
+            borderRadius: 20,
+            height: "800px",
+          }}
+        >
+          <h1 style={{ textAlign: "center", color: "red" }}>
+            LỊCH SỬ HÌNH THÀNH VÀ PHÁT TRIỂN CỦA IPHONE
+          </h1>
+          <div className="chill">
+            <div style={{ float: "left", width: "50%", lineHeight: "40px" }}>
+              iPhone là dòng điện thoại thông minh được phát triển từ Apple Inc,
+              được ra mắt lần đầu tiên bởi Steve Jobs và mở bán năm 2007. Bên
+              cạnh tính năng của một máy điện thoại thông thường, iPhone còn
+              được trang bị màn hình cảm ứng, camera, khả năng chơi nhạc và
+              chiếu phim, trình duyệt web... Phiên bản thứ hai là iPhone 3G ra
+              mắt tháng 7 năm 2008, được trang bị thêm hệ thống định vị toàn
+              cầu, mạng 3G tốc độ cao.
+            </div>
+            <img className="image4" src={anh4} alt="" />
+          </div>
+          <hr />
+          <h1 style={{ textAlign: "center", color: "red" }}>
+            IPHONE CÓ NHỮNG MÃ MÁY NÀO?
+          </h1>
+          <div className="chill">
+            <div style={{ float: "right", width: "50%", lineHeight: "40px" }}>
+              Những chiếc iPhone do Apple phân phối tại thị trường nước nào thì
+              sẽ mang mã của nước đó. Ví dụ: LL: Mỹ, ZA: Singapore, TH: Thái
+              Lan, JA: Nhật Bản, Những mã này xuất hiện tại Việt Nam đều là hàng
+              xách tay, nhập khẩu. Còn tại Việt Nam, iPhone sẽ được mang mã
+              VN/A. Tất cả các mã này đều là hàng chính hãng phân phối của
+              Apple. Lợi thế khi bạn sử dụng iPhone mã VN/A đó là chế độ bảo
+              hành tốt hơn với 12 tháng theo tiêu chuẩn của Apple. iPhone của
+              bạn sẽ được bảo hành tại tất cả các trung tâm bảo hành Apple tại
+              Việt Nam, một số mã quốc tế bị từ chối bảo hành và phải đem ra các
+              trung tâm bảo hành Apple tại nước ngoài. Rất là phức tạp đúng
+              không nào?
+            </div>
+            <img className="image5" src={anh5} alt="" />
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
--- my-project/src/components/News.jsx
+++ my-project/src/components/News.jsx
@@ -1,60 +1,67 @@
-import React from 'react'
-import "../index.css"
+import React from "react";
+import "../index.css";
 
 const newsData = [
-    {
-      id: 1,
-      title: 'Apple ra mắt iPhone 15 mới',
-      content: 'Apple đã công bố phiên bản mới nhất của dòng sản phẩm iPhone, mang tên iPhone 15, với nhiều cải tiến về hiệu năng và camera.',
-      image: 'https://vcdn-sohoa.vnecdn.net/2023/02/27/iPhone-15-Pro-Burgandy-Feature-9729-9375-1677434034.jpg',
-    },
-    {
-      id: 2,
-      title: 'Theo một số đồn đoán, các mẫu iPhone 15 Pro có thể tăng giá',
-      content: 'Theo Jeff Pu - nhà phân tích công nghệ tại Haitong International Securities, iPhone 15 Pro và iPhone 15 Pro Max sắp ra mắt của Apple có thể đi kèm với mức giá cao hơn do những nâng cấp đáng kể về phần cứng.',
-      image: 'https://cdn.mobilecity.vn/mobilecity-vn/images/2023/03/theo-mot-so-don-doan-cac-mau-iphone-15-pro-co-the-tang-gia1.jpg.webp',
-    },
-    {
-      id:3,
-      title:'iPhone 15 Plus có gì mới?',
-      content:'Phiên bản bộ nhớ: iPhone 15 Plus 128GB, iPhone 15 Plus 256GB, iPhone 15 Plus 512GB.Màu sắc iPhone 15 Plus: Đen, xanh dương, hồng, tím, trắng và đỏ.Thiết kế iPhone 15 Plus: Cũng như iPhone 15 tiêu chuẩn, phần tai thỏ sẽ hoàn toàn được thay thay thế bằng thiết kế màn hình đục lỗ hình viên thuốc được Apple gọi là Dynamic Island.',
-      image:'https://cdn.tgdd.vn/Files/2023/03/17/1518606/ogmacrumorscopy-100423-205920.jpg',
-    },
-    {
-      id:4,
-      title:'iPhone 15 Series có Chip Apple A17 Bionic với quy trình 3nm mới',
-      content:'Giống với iPhone 14 series, iPhone 15 cũng sẽ được Apple tiếp tục trang bị bộ vi xử lý tương tự. Tức có nghĩa là chip Apple A16 Bionic 4nm sẽ được dùng trên iPhone 15 và iPhone 15 Plus, còn các dòng Pro sẽ được trang bị con chip Apple A17 Bionic 3nm đời mới nhất..',
-      image:'https://blogcdn.muaban.net/wp-content/uploads/2023/05/24225856/iPhone-15-khi-nao-ra-mat-09.jpg',
-    },
-    // Thêm các tin tức khác tại đây...
-  ];
-  
-  // Component tin tức
-  function NewsItem({ title, content, image }) {
-    return (
-      <div className="news-item">
-        <img className="news-image" src={image} alt={title} />
-        <div className="news-content">
-          <h2>{title}</h2>
-          <p>{content}</p>
-        </div>
+  {
+    id: 1,
+    title: "Apple ra mắt iPhone 15 mới",
+    content:
+      "Apple đã công bố phiên bản mới nhất của dòng sản phẩm iPhone, mang tên iPhone 15, với nhiều cải tiến về hiệu năng và camera.",
+    image:
+      "https://vcdn-sohoa.vnecdn.net/2023/02/27/iPhone-15-Pro-Burgandy-Feature-9729-9375-1677434034.jpg",
+  },
+  {
+    id: 2,
+    title: "Theo một số đồn đoán, các mẫu iPhone 15 Pro có thể tăng giá",
+    content:
+      "Theo Jeff Pu - nhà phân tích công nghệ tại Haitong International Securities, iPhone 15 Pro và iPhone 15 Pro Max sắp ra mắt của Apple có thể đi kèm với mức giá cao hơn do những nâng cấp đáng kể về phần cứng.",
+    image:
+      "https://cdn.mobilecity.vn/mobilecity-vn/images/2023/03/theo-mot-so-don-doan-cac-mau-iphone-15-pro-co-the-tang-gia1.jpg.webp",
+  },
+  {
+    id: 3,
+    title: "iPhone 15 Plus có gì mới?",
+    content:
+      "Phiên bản bộ nhớ: iPhone 15 Plus 128GB, iPhone 15 Plus 256GB, iPhone 15 Plus 512GB.Màu sắc iPhone 15 Plus: Đen, xanh dương, hồng, tím, trắng và đỏ.Thiết kế iPhone 15 Plus: Cũng như iPhone 15 tiêu chuẩn, phần tai thỏ sẽ hoàn toàn được thay thay thế bằng thiết kế màn hình đục lỗ hình viên thuốc được Apple gọi là Dynamic Island.",
+    image:
+      "https://cdn.tgdd.vn/Files/2023/03/17/1518606/ogmacrumorscopy-100423-205920.jpg",
+  },
+  {
+    id: 4,
+    title: "iPhone 15 Series có Chip Apple A17 Bionic với quy trình 3nm mới",
+    content:
+      "Giống với iPhone 14 series, iPhone 15 cũng sẽ được Apple tiếp tục trang bị bộ vi xử lý tương tự. Tức có nghĩa là chip Apple A16 Bionic 4nm sẽ được dùng trên iPhone 15 và iPhone 15 Plus, còn các dòng Pro sẽ được trang bị con chip Apple A17 Bionic 3nm đời mới nhất..",
+    image:
+      "https://blogcdn.muaban.net/wp-content/uploads/2023/05/24225856/iPhone-15-khi-nao-ra-mat-09.jpg",
+  },
+  // Thêm các tin tức khác tại đây...
+];
+
+// Component tin tức
+function NewsItem({ title, content, image }) {
+  return (
+    <div className="news-item">
+      <img className="news-image" src={image} alt={title} />
+      <div className="news-content">
+        <h2>{title}</h2>
+        <p>{content}</p>
       </div>
-    );
-  }
+    </div>
+  );
+}
 export default function News() {
   return (
     <div className="news-page">
-    <h1>Tin tức điện thoại</h1>
-    <div className="news-container">
-      {newsData.map((item) => (
-        <NewsItem
-          key={item.id}
-          title={item.title}
-          content={item.content}
-          image={item.image}
-        />
-      ))}
+      <div className="news-container">
+        {newsData.map((item) => (
+          <NewsItem
+            key={item.id}
+            title={item.title}
+            content={item.content}
+            image={item.image}
+          />
+        ))}
+      </div>
     </div>
-  </div>
-  )
+  );
 }
